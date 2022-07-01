3aa374158b9b161d1f727c5ba07459c6b6a4424d
andrewbrennanfr <20241062+andrewbrennanfr@users.noreply.github.com>
2022-07-01 22:00:07 +0200

    :art: Implement default CSS

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,43 @@
 <template>
     <div>App</div>
 </template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300&display=swap");
+
+* {
+    appearance: none;
+    background: none;
+    border: none;
+    box-sizing: border-box;
+    color: inherit;
+    font: inherit;
+    list-style: none;
+    margin: 0;
+    outline: none;
+    padding: 0;
+    text-decoration: none;
+}
+
+body {
+    font: 400 1.6rem/1.8 "Nunito Sans", sans-serif;
+}
+
+body,
+html {
+    height: 100%;
+    width: 100%;
+}
+
+html {
+    font: normal 0.625em/1 -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+        Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+        "Segoe UI Symbol";
+}
+
+main {
+    display: block;
+    min-height: 100%;
+    width: 100%;
+}
+</style>
